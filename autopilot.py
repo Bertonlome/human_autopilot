ee21f34e84f77c5ace32d67df07e563f9b29339c
ben <you@example.com>
2025-06-17 16:17:13 -0400

    work

--- autopilot.py
+++ autopilot.py
@@ -15,24 +15,29 @@ def normalize(value, min=-1, max=1):
     if (value > max):
         return max
     elif (value < min):
-        return min
+        return min  
     else:
         return value
 
-update_interval = 0.050 # seconds, 0.05 = 20 Hz
+update_interval = 0.025 # seconds, 0.05 = 20 Hz
 start = datetime.now()
 last_update = start
 
 # defining the initial PID values
-P = 0.1 # PID library default = 0.2
-I = P/10 # default = 0
+P_pitch = 0.1 # PID library default = 0.2
+P_roll = 0.01
+P_skid = 0.01
+I_pitch = P_pitch/10 # default = 0
+I_roll = P_roll/10
+I_skid = P_skid/10
 D = 0 # default = 0
 
 # initializing PID controllers
-roll_PID = PID.PID(P, I, D)
-pitch_PID = PID.PID(P, I, D)
-altitude_PID = PID.PID(P, I, D)
-skid_PID = PID.PID(P, I, D)
+roll_PID = PID.PID(P_roll, I_roll, D)
+pitch_PID = PID.PID(P_pitch, I_pitch, D)
+altitude_PID = PID.PID(P_pitch, I_pitch, D)
+skid_PID = PID.PID(P_skid, I_skid, D)
+rudder_PID = PID.PID(P_skid, I_skid, D)
 
 
 
@@ -40,9 +45,10 @@ skid_PID = PID.PID(P, I, D)
 # roll = 0 means wings level
 # pitch = 2 means slightly nose up, which is required for level flight
 desired_roll = 0
-desired_pitch = 5
+desired_pitch = 10
 desired_altitude = 4500
 desired_skid = 0
+
 
 # # setting the PID set points with our desired values
 pitch_PID.SetPoint = desired_pitch
@@ -66,7 +72,7 @@ DREFs = ["sim/cockpit2/gauges/indicators/airspeed_kts_pilot",
         "sim/flightmodel/controls/parkbrake",
         "sim/cockpit2/engine/actuators/throttle_ratio_all"
         ]
-start = time.time()
+
 def monitor():
     global i
     global last_update
@@ -90,8 +96,8 @@ def monitor():
                 current_altitude = multi_DREFs[3][0]
                 current_asi = multi_DREFs[0][0]
                 onground = multi_DREFs[2][0]
-                current_rudder = client.getDREF("sim/joystick/yoke_heading_ratio")[0]
-                current_skid = client.getDREF("sim/flightmodel/position/beta")[0]
+                current_rudder = ctrl[2]
+                current_skid = client.getDREF("sim/cockpit2/gauges/indicators/slip_deg")[0]
 
                 print("Rudder position:", current_rudder)
                 print("Skid position:", current_skid)
@@ -103,10 +109,10 @@ def monitor():
                     time.sleep(15)
                     start = False
                     
-                print(current_asi)
+                #print(current_asi)
                 if(current_asi > SPEED):  
                     if(current_altitude > 1900 and hwg): #and bearing difference > 30  #and input igs through roll_PID.user_request_a_roll
-                        marge_derreur = 5 # 5 degrees of error
+                        marge_derreur = 2 # 5 degrees of error
                         user_requested_heading = 80 #roll_PID.user_requested_heading
                         current_heading = current_hdg
                         # Calcul de la différence minimale d'angle
@@ -126,30 +132,31 @@ def monitor():
                     
                     # update outer loops first
                     altitude_PID.update(current_altitude)
-#
+                    skid_PID.update(current_skid)
                     # if alt=12000, setpoint = 10000, the error is 2000. if P=0.1, output will be 2000*0.1=200
-                    pitch_PID.SetPoint = normalize(altitude_PID.output, min=-15, max=5)
-
+                    pitch_PID.SetPoint = normalize(altitude_PID.output, min=-15, max=10)
+                    rudder_PID.SetPoint = normalize(skid_PID.output)
+                    
                     # update PIDs
                     roll_PID.update(current_roll)
                     pitch_PID.update(current_pitch)
-                    skid_PID.update(current_rudder)
-                    skid_PID.update(current_skid)
+                    rudder_PID.update(current_rudder)
+                    #skid_PID.update(current_skid)
 
                     # update control outputs
                     new_ail_ctrl = normalize(roll_PID.output)
                     new_ele_ctrl = normalize(pitch_PID.output)
-                    new_skid_ctrl = normalize(skid_PID.output,min=-1,max=1)
+                    new_rudder_ctrl = normalize(rudder_PID.output,min=-1,max=1)
 
                     # sending actual control values to XPlane
                     
-                    ctrl = [new_ele_ctrl, new_ail_ctrl, new_skid_ctrl , -998] # -998 pour ne pas changer le moteur
+                    ctrl = [new_ele_ctrl, new_ail_ctrl, new_rudder_ctrl , -998] # -998 pour ne pas changer le moteur
                     client.sendCTRL(ctrl)
 
 
                     output = f"current values --    roll: {current_roll: 0.3f},  pitch: {current_pitch: 0.3f}"
                     output = output + "\n" + f"PID outputs    --    roll: {roll_PID.output: 0.3f},  pitch: {pitch_PID.output: 0.3f}"
                     output = output + "\n"
-                    print(output)
+                    #print(output)
 if __name__ == "__main__":
     monitor()
